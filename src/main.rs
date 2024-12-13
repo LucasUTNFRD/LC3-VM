bf56d0257f15b998c53096de2df20ffe17d3a2df
LucasUTNFRD <ldelgado@frd.utn.edu.ar>
2024-12-13 13:47:49 -0300

    Implement  Opcode enum

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,18 @@
 // Look at the opcode to determine which type of instruction it should perform.
 // Perform the instruction using the parameters in the instruction.
 // Go back to step 1.
+mod memory;
+mod opdcodes;
+mod registers;
 fn main() {
-    println!("Hello, world!");
+    // Read the program file given as the first command line argument
+    // This will be used ./lc3-vm path/to/program.obj
+    // ensure that the argument is passed and the file is readable
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {:?} [image-file1] ...", args.first());
+    }
+
+    // TODO: Load the program into memory
 }
---
+++ src/opdcodes.rs
@@ -0,0 +1,18 @@
+pub enum Opcode {
+    Br = 0, /* branch */
+    Add,    /* add  */
+    Ld,     /* load */
+    St,     /* store */
+    Jsr,    /* jump register */
+    And,    /* bitwise and */
+    Ldr,    /* load register */
+    Str,    /* store register */
+    Rti,    /* unused */
+    Not,    /* bitwise not */
+    Ldi,    /* load indirect */
+    Sti,    /* store indirect */
+    Jmp,    /* jump */
+    Res,    /* reserved (unused) */
+    Lea,    /* load effective address */
+    Trap,   /* execute trap */
+}
