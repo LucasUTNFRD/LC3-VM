1b289cbb991843b3acbdd9b1c6a2d07e69cfe264
LucasUTNFRD <ldelgado@frd.utn.edu.ar>
2024-12-13 13:12:52 -0300

    Add Cargo.toml config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// Load one instruction from memory at the address of the PC register.
+// Increment the PC register.
+// Look at the opcode to determine which type of instruction it should perform.
+// Perform the instruction using the parameters in the instruction.
+// Go back to step 1.
 fn main() {
     println!("Hello, world!");
 }
